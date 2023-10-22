efec17690e4409960846654dd5f4d822c9e2f50d
medalha01 <hiperboss2009@gmail.com>
2023-10-22 12:26:51 -0300

    Importing and setting main

--- kojun/src/main/scala/Main.scala
+++ kojun/src/main/scala/Main.scala
@@ -1,5 +1,31 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+object Main extends App {
+  import scala.io.StdIn.readLine
+  import data_structures.data.Tabuleiros._
 
-def msg = "I was compiled by Scala 3. :)"
+  println(
+    "Digite o tamanho do tabuleiro que quer que seja resolvido 6, 8 ou 10:"
+  )
+  val input = readLine()
+  val tamanho = input.toInt
+
+  println("\n")
+  tamanho match {
+    case 6 =>
+      val resultadoIO =
+        kojun(getValoresTabuleiro6x6, getRegioesTabuleiro6x6, tamanho)
+      println(resultadoIO)
+    case 8 =>
+      val resultadoIO =
+        kojun(getValoresTabuleiro8x8, getRegioesTabuleiro8x8, tamanho)
+      println(resultadoIO)
+    case 10 =>
+      val resultadoIO =
+        kojun(getValoresTabuleiro10x10, getRegioesTabuleiro10x10, tamanho)
+      println(resultadoIO)
+    case _ =>
+      println(
+        "Não existe um tabuleiro desse tamanho no banco. Tente novamente."
+      )
+      Main.main(Array())
+  }
+}
--- kojun/src/main/scala/data.scala
+++ kojun/src/main/scala/data.scala
@@ -1,3 +1,5 @@
+package data_structures.data
+
 object Tabuleiros {
 
   // Type Definitions
