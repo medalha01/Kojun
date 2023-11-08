684ae4435a8d6d8de4caaa79e7be9cffd1fc0204
medalha01 <hiperboss2009@gmail.com>
2023-11-08 19:09:56 -0300

    Comentando código

--- kojun/src/main/scala/solver/KojunSolver.scala
+++ kojun/src/main/scala/solver/KojunSolver.scala
@@ -1,21 +1,27 @@
+// Definição do pacote ao qual a classe pertence
 package solver
 
+// Importações de classes e métodos necessários
 import utils.Matrix
-
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable
 import scala.language.postfixOps
 
+// Definição da classe MyKojunSolver com duas matrizes como parâmetros: uma de valores e outra de regiões
 class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
+  // Tipo Position é definido como um par de Inteiros para representar posições na matriz
   private type Position = (Int, Int)
 
+  // regionMapping é um mapa mutável que liga strings (identificadores de região) a listas de posições
   private val regionMapping: mutable.Map[String, List[(Int, Int)]] =
     createRegionMapping
 
+  // Método para criar o mapeamento das regiões da matriz de regiões para suas respectivas posições
   private def createRegionMapping: mutable.Map[String, List[(Int, Int)]] = {
     val regionMapping = mutable.Map[String, ListBuffer[(Int, Int)]]()
 
+    // Loop para percorrer as células da matriz de regiões e preencher o mapeamento
     for {
       row <- 0 until regionMatrix.numRows
       col <- 0 until regionMatrix.numCols
@@ -24,6 +30,7 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
       regionMapping.getOrElseUpdate(regionValue, ListBuffer()) += ((row, col))
     }
 
+    // Conversão de ListBuffer para List após o término do loop
     val result = regionMapping.map { case (regionValue, positionsBuffer) =>
       regionValue -> positionsBuffer.toList
     }
@@ -31,6 +38,7 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
     result
   }
 
+  // Método para verificar se é possível inserir um valor em uma determinada posição da matriz
   private def canInsertValue(
       position: Position,
       value: Int,
@@ -38,10 +46,14 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
       regionMatrix: Matrix[String],
       regionMapping: mutable.Map[String, List[(Int, Int)]]
   ): Boolean = {
+    // Desconstrução da posição em linha e coluna
     val (row, col) = position
+    // Obter o valor da região na posição atual
     val regionValue = regionMatrix.getMatrixValue(position)
+    // Obter todas as posições na mesma região
     val regionPositions = regionMapping(regionValue)
 
+    // Obter os valores adjacentes à posição atual, excluindo posições inválidas
     val adjacentValues = List(
       (row - 1, col),
       (row + 1, col),
@@ -49,36 +61,42 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
       (row, col + 1)
     ).filter(valueMatrix.isValidPosition).map(valueMatrix.getMatrixValue)
 
+    // Validar se o valor é menor que o da posição acima dentro da mesma região, se aplicável
     val isTopValid =
       if (
-        valueMatrix
-          .isValidPosition((row - 1, col)) && regionMatrix.getMatrixValue(
-          (row - 1, col)
-        ) == regionValue
+        valueMatrix.isValidPosition((row - 1, col)) && regionMatrix
+          .getMatrixValue((row - 1, col)) == regionValue
       ) {
         value < valueMatrix.getMatrixValue((row - 1, col))
       } else true
 
+    // Validar se o valor é maior que o da posição abaixo dentro da mesma região, se aplicável
     val isBottomValid =
       if (
-        valueMatrix
-          .isValidPosition((row + 1, col)) && regionMatrix.getMatrixValue(
-          (row + 1, col)
-        ) == regionValue
+        valueMatrix.isValidPosition((row + 1, col)) && regionMatrix
+          .getMatrixValue((row + 1, col)) == regionValue
       ) {
         value > valueMatrix.getMatrixValue((row + 1, col))
       } else true
 
+    // Retornar se todas as condições de validação são atendidas
     !(
-      valueMatrix.getMatrixValue(position) != 0 ||
-        value < 1 || value > regionPositions.length ||
-        adjacentValues.contains(value) ||
-        regionPositions.map(valueMatrix.getMatrixValue).contains(value) ||
-        !isTopValid ||
-        !isBottomValid
+      valueMatrix.getMatrixValue(
+        position
+      ) != 0 || // A posição atual deve estar vazia
+        value < 1 || value > regionPositions.length || // O valor deve estar dentro do tamanho da região
+        adjacentValues.contains(
+          value
+        ) || // O valor não deve ser igual a nenhum valor adjacente
+        regionPositions
+          .map(valueMatrix.getMatrixValue)
+          .contains(value) || // O valor deve ser único na região
+        !isTopValid || // Validação do topo
+        !isBottomValid // Validação da base
     )
   }
 
+  // Método recursivo para tentar inserir valores possíveis em uma posição da matriz
   @tailrec
   private def tryValues(
       row: Int,
@@ -88,10 +106,13 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
       regionMatrix: Matrix[String],
       regionMapping: mutable.Map[String, List[(Int, Int)]]
   ): Option[Matrix[Int]] = {
+    // Se não houver mais valores para tentar, retorna None
     if (valuesToTry.isEmpty) {
       None
     } else {
+      // Pega o primeiro valor da lista para tentar
       val value = valuesToTry.head
+      // Se for possível inserir o valor na posição atual
       if (
         canInsertValue(
           (row, col),
@@ -101,17 +122,22 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
           regionMapping
         )
       ) {
+        // Insere o valor na matriz
         val updatedValueMatrix = valueMatrix.setMatrixValue((row, col), value)
+        // Determina a próxima posição para tentar
         val nextPosition =
           if (col == valueMatrix.numCols - 1) (row + 1, 0) else (row, col + 1)
+        // Chama solveKojun recursivamente para resolver o próximo passo
         solveKojun(
           updatedValueMatrix,
           regionMatrix,
           nextPosition,
           regionMapping
         ) match {
-          case Some(solution) => Some(solution)
+          case Some(solution) =>
+            Some(solution) // Se encontrou uma solução, retorna-a
           case None =>
+            // Se não encontrou solução, tenta com o próximo valor
             tryValues(
               row,
               col,
@@ -122,6 +148,7 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
             )
         }
       } else {
+        // Se o valor atual não for válido, tenta o próximo
         tryValues(
           row,
           col,
@@ -134,6 +161,7 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
     }
   }
 
+  // Método recursivo principal para resolver o quebra-cabeça
   @tailrec
   private def solveKojun(
       valueMatrix: Matrix[Int],
@@ -142,19 +170,24 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
       regionMapping: mutable.Map[String, List[(Int, Int)]]
   ): Option[Matrix[Int]] = {
     val (row, col) = position
+    // Se chegou ao fim da matriz, retorna a matriz como solução
     if (row == valueMatrix.numRows) {
       Some(valueMatrix)
     } else if (col == valueMatrix.numCols) {
+      // Se chegou ao fim da coluna, passa para a próxima linha
       solveKojun(valueMatrix, regionMatrix, (row + 1, 0), regionMapping)
     } else if (
       valueMatrix
         .isValidPosition(position) && valueMatrix.getMatrixValue(position) != 0
     ) {
+      // Se a posição atual já tiver um valor, passa para a próxima coluna
       solveKojun(valueMatrix, regionMatrix, (row, col + 1), regionMapping)
     } else {
+      // Determina o tamanho máximo da região para a posição atual
       val maxRegionSize = regionMapping(
         regionMatrix.getMatrixValue(position)
       ).length
+      // Tenta valores de 1 até o tamanho máximo da região para a posição atual
       tryValues(
         row,
         col,
@@ -166,6 +199,7 @@ class MyKojunSolver(valueMatrix: Matrix[Int], regionMatrix: Matrix[String]) {
     }
   }
 
+  // Método público para resolver o quebra-cabeça; começa a resolução pela primeira posição da matriz
   def solve(): Option[Matrix[Int]] = {
     solveKojun(valueMatrix, regionMatrix, (0, 0), regionMapping)
   }
